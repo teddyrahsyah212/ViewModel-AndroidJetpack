3e8926aefa8a10948288b9c8820c35283e10ae81
teddyrahsyah212 <teddyzoeldyek@gmail.com>
2021-04-20 17:19:13 +0700

    first commit (Dicoding Lecture)

---
+++ app/src/main/java/id/develo/myviewmodel/MainActivity.kt
@@ -0,0 +1,48 @@
+package id.develo.myviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import id.develo.myviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih Kosong"
+                }
+                height.isEmpty() -> {
+                    binding.edtWidth.error = "Masih Kosong"
+                }
+                length.isEmpty() -> {
+                    binding.edtWidth.error = "Masih Kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
+    }
+}
